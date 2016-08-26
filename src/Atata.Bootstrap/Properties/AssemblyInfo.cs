89b65d7bdbbcbcc7d3d379dfa79363860fb664b2
Yevgeniy Shunevych <eshunevich@gmail.com>
2016-08-26 22:31:35 +0300

    Add basic project files

---
+++ src/Atata.Bootstrap/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Atata.Bootstrap")]
+[assembly: AssemblyCompany("Yevgeniy Shunevych")]
+[assembly: AssemblyProduct("Atata.Bootstrap")]
+[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2016")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ad6d1f54-007e-4c7d-8a2b-4a176cf00484")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
