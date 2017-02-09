72a71864fee77b54ab9b32afcf882ea183a0b7d3
YevgeniyShunevych <eshunevich@gmail.com>
2017-02-09 18:30:50 +0200

    Override GetIsEnabled method in BSNavItem`1 control; Add BSClass static class

---
+++ src/Atata.Bootstrap/BSClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Atata.Bootstrap
+{
+    public static class BSClass
+    {
+        public const string Active = "active";
+        public const string Disabled = "disabled";
+    }
+}
--- src/Atata.Bootstrap/BSNavItem`1.cs
+++ src/Atata.Bootstrap/BSNavItem`1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using OpenQA.Selenium;
 
 namespace Atata.Bootstrap
 {
@@ -8,6 +9,13 @@ namespace Atata.Bootstrap
     {
         public DataProvider<bool, TOwner> IsActive => GetOrCreateDataProvider(
             nameof(IsActive).ToString(TermCase.MidSentence),
-            () => Attributes.Class.Value.Contains("active"));
+            () => Attributes.Class.Value.Contains(BSClass.Active));
+
+        protected override bool GetIsEnabled()
+        {
+            // In 3rd version of Bootstrap 'disabled' class applies to the <li> element, but in 4th - to the <a>.
+            IWebElement scope = Scope;
+            return !scope.HasClass(BSClass.Disabled) && !(scope.Get(By.TagName("a").SafelyAtOnce())?.HasClass(BSClass.Disabled) ?? false);
+        }
     }
 }
