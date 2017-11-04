583195b5af550b47eb738206e2fd814b5cf123f4
youngmtool <youngmtool@gmail.com>
2017-11-04 13:15:27 +0900

    Update comments

--- src/Atata.Bootstrap/BSAccordion`1.cs
+++ src/Atata.Bootstrap/BSAccordion`1.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Atata.Bootstrap
 {
+    /// <summary>
+    /// Represents the accordion item control(Bootstrap data-parent="#accordion"). Default search finds the first occuring data-parent="#accordion" element.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     // [ControlDefinition("a[@data-toggle='collapse' AND @data-parent='#accordion']", ComponentTypeName = "collapse with accordion")]
     [ControlDefinition("a[@data-parent='#accordion']", ComponentTypeName = "collapse with accordion")]
     public class BSAccordion<TOwner> : Control<TOwner>
--- src/Atata.Bootstrap/BSCollapseWithButton`1.cs
+++ src/Atata.Bootstrap/BSCollapseWithButton`1.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Atata.Bootstrap
 {
+    /// <summary>
+    /// Represents the collapse with button item control(Bootstrap <button data-toggle="collapse" />). Default search finds the first occuring data-toggle="collapse" element.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("button[@data-toggle='collapse']", ComponentTypeName = "collapse with button")]
     public class BSCollapseWithButton<TOwner> : Control<TOwner>
         where TOwner : PageObject<TOwner>
