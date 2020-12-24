65cbd3c6207f129ecfd7136900a7b4fb4939697d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-12-24 14:27:38 +0200

    #27 Update BSNavItem<TOwner> IsEnabled and IsActive implementations to handle cases of Bootstrap v4 and v5

--- src/Atata.Bootstrap/BSNavItem`1.cs
+++ src/Atata.Bootstrap/BSNavItem`1.cs
@@ -1,22 +1,28 @@
-﻿using System.Linq;
-using OpenQA.Selenium;
-
-namespace Atata.Bootstrap
+﻿namespace Atata.Bootstrap
 {
-    [ControlDefinition("li", ComponentTypeName = "nav item")]
+    [ControlDefinition(ComponentTypeName = "nav item")]
     [FindByContent]
     public class BSNavItem<TOwner> : Control<TOwner>
         where TOwner : PageObject<TOwner>
     {
         public DataProvider<bool, TOwner> IsActive => GetOrCreateDataProvider(
             "active state",
-            () => Attributes.Class.Value.Contains(BSClass.Active));
+            GetIsActive);
+
+        [FindByXPath("(self::* | child::*)[contains(concat(' ', normalize-space(@class), ' '), ' active ')]")]
+        protected Control<TOwner> ActiveIdentifier { get; private set; }
+
+        [FindByXPath("(self::* | child::*)[contains(concat(' ', normalize-space(@class), ' '), ' disabled ')]")]
+        protected Control<TOwner> DisabledIdentifier { get; private set; }
+
+        protected virtual bool GetIsActive()
+        {
+            return ActiveIdentifier.IsPresent;
+        }
 
         protected override bool GetIsEnabled()
         {
-            // In 3rd version of Bootstrap 'disabled' class applies to the <li> element, but in 4th - to the <a>.
-            IWebElement scope = Scope;
-            return !scope.HasClass(BSClass.Disabled) && !(scope.Get(By.TagName("a").SafelyAtOnce())?.HasClass(BSClass.Disabled) ?? false);
+            return !DisabledIdentifier.IsPresent;
         }
     }
 }
