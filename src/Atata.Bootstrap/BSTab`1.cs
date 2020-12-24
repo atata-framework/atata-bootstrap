a916a95a01c20e8381a229df79e1a09c315ba6d3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-12-24 14:21:56 +0200

    #26 Make ControlDefinition XPath of BSTab<TOwner> less strict

--- src/Atata.Bootstrap/BSTab`1.cs
+++ src/Atata.Bootstrap/BSTab`1.cs
@@ -1,6 +1,6 @@
 ﻿namespace Atata.Bootstrap
 {
-    [ControlDefinition("li[parent::ul[contains(concat(' ', normalize-space(@class), ' '), ' nav-tabs ')]]", ComponentTypeName = "tab")]
+    [ControlDefinition("*[parent::*[contains(concat(' ', normalize-space(@class), ' '), ' nav-tabs ')]]", ComponentTypeName = "tab")]
     public class BSTab<TOwner> : BSNavItem<TOwner>
         where TOwner : PageObject<TOwner>
     {
