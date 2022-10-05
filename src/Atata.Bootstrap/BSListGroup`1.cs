792a1f7e44ecb3fdc737ed2404d7e7aba1af6674
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-10-05 12:23:43 +0200

    Correct typo in `BSListGroup<TOwner>` XML comment

--- src/Atata.Bootstrap/BSListGroup`1.cs
+++ src/Atata.Bootstrap/BSListGroup`1.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Represents Bootstrap list group control.
-    /// Default search finds the first occuring element with "list-group" class.
+    /// Default search finds the first occurring element with "list-group" class.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     /// <seealso cref="BSListGroupItem{TOwner}" />
