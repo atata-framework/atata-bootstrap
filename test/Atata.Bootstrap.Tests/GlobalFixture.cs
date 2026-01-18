2f25227c11b1a96c3c26024056407e6620b0aa19
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-01-18 18:03:37 +0100

    Update test project to use Atata.AspNetCore.v10 package to start test web application

--- test/Atata.Bootstrap.Tests/GlobalFixture.cs
+++ test/Atata.Bootstrap.Tests/GlobalFixture.cs
@@ -1,14 +1,10 @@
 ﻿using System.Net.NetworkInformation;
-using Atata.Cli;
-using Atata.WebDriverSetup;
 
 namespace Atata.Bootstrap.Tests;
 
 [SetUpFixture]
 public class GlobalFixture : AtataGlobalFixture
 {
-    private CliCommand? _dotnetRunCommand;
-
     protected override void OnBeforeGlobalSetup() =>
         ThreadPool.SetMinThreads(Environment.ProcessorCount * 4, Environment.ProcessorCount);
 
@@ -25,43 +21,21 @@ public class GlobalFixture : AtataGlobalFixture
                     "disable-search-engine-choice-screen")));
     }
 
-    [OneTimeSetUp]
-    public async Task GlobalSetUpAsync() =>
-        await Task.WhenAll(
-            DriverSetup.AutoSetUpAsync(BrowserNames.Chrome),
-            Task.Run(SetUpTestApp));
-
-    private static bool IsTestAppRunning() =>
-        Array.Exists(
-            IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners(),
-            x => x.Port == UITestSuite.TestAppPort);
-
-    private void SetUpTestApp()
+    protected override void ConfigureGlobalAtataContext(AtataContextBuilder builder)
     {
+        builder.SetUpWebDriversForUse();
+
         if (!IsTestAppRunning())
-            StartTestApp();
-    }
-
-    private void StartTestApp()
-    {
-        string testAppPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "Atata.Bootstrap.TestApp");
-
-        ProgramCli dotnetCli = new ProgramCli("dotnet", useCommandShell: true)
-            .WithWorkingDirectory(testAppPath);
-
-        _dotnetRunCommand = dotnetCli.Start("run");
-
-        RetryWait testAppWait = new(TimeSpan.FromSeconds(40), TimeSpan.FromSeconds(0.2));
-        testAppWait.Until(IsTestAppRunning);
-    }
-
-    [OneTimeTearDown]
-    public void GlobalTearDown()
-    {
-        if (_dotnetRunCommand is not null)
         {
-            _dotnetRunCommand.Kill(true);
-            _dotnetRunCommand.Dispose();
+            builder.Sessions.AddWebApplication(x => x
+                .Use<Program>()
+                .UseKestrel(UITestSuite.TestAppPort)
+                .UseMinimumApplicationLogLevel(Microsoft.Extensions.Logging.LogLevel.Warning));
         }
     }
+
+    private static bool IsTestAppRunning() =>
+        IPGlobalProperties.GetIPGlobalProperties()
+            .GetActiveTcpListeners()
+            .Any(x => x.Port == UITestSuite.TestAppPort);
 }
